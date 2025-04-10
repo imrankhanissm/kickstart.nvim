217dda383822d5f7d499d375668672002402a748
imran <imran@urexam.in>
2025-04-10 18:31:51 +0200

    search symbols

--- init.lua
+++ init.lua
@@ -412,7 +412,6 @@ require('lazy').setup({
         pickers = {
           buffers = {
             sort_lastused = true,
-            ignore_current_buffer = true,
             mappings = {
               i = {
                 ['<C-d>'] = 'delete_buffer',
@@ -440,7 +439,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = '[S]earch [S]ymbols' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
