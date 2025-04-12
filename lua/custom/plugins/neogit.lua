87b1088463f0522082acdbf82b45c5e7ca05dbba
imran <imran@urexam.in>
2025-04-12 09:59:05 +0200

    diffview toggle

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -5,8 +5,26 @@ return {
     {
       'sindrets/diffview.nvim', -- optional - Diff integration
       config = function()
-        vim.keymap.set('n', '<leader>dvo', '<cmd>DiffviewOpen<CR>', { desc = '[D]iff[V]iew [O]pen' })
-        vim.keymap.set('n', '<leader>dvc', '<cmd>DiffviewClose<CR>', { desc = '[D]iff[V]iew [C]lose' })
+        vim.keymap.set('n', '<leader>dv', function()
+          local function is_diffview_tab()
+            for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+              if ft == 'DiffviewFiles' or ft == 'DiffviewView' then
+                return true
+              end
+            end
+            return false
+          end
+
+          if is_diffview_tab() then
+            print 'dv open'
+            vim.cmd 'DiffviewClose'
+          else
+            print 'dv close'
+            vim.cmd 'DiffviewOpen'
+          end
+        end, { desc = '[D]iff[V]iew' })
       end,
     },
 
