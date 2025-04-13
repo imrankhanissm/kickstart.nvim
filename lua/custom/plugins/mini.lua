c20f6182f2f233b3435d530f1786c68f49006fbf
imran <imran@urexam.in>
2025-04-13 08:58:12 +0200

    mini files

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,9 +2,21 @@ return {
   'echasnovski/mini.files',
   version = '*',
   config = function()
-    local mini_files = require('mini.files').setup()
+    require('mini.files').setup {
+      windows = {
+        -- Width of focused window
+        width_focus = 50,
+        -- Width of non-focused window
+        width_nofocus = 50,
+        -- Width of preview window
+        width_preview = 50,
+      },
+    }
     vim.keymap.set('n', '<leader>e', function()
       MiniFiles.open(vim.api.nvim_buf_get_name(0))
-    end, { desc = 'Mini Files [E]xplorer' })
+    end, { desc = 'Mini Files [E]xplorer (current file directory)' })
+    vim.keymap.set('n', '<leader>E', function()
+      MiniFiles.open()
+    end, { desc = 'Mini Files [E]xplorer (root directory)' })
   end,
 }
